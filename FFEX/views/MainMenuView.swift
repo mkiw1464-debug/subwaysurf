eeed2fe2644a53f6af191cfa19a897ca2176c4be
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-17 03:25:49 -0700

    Fix formatting and add newline at end of MainMenuView.swift

--- FFEX/views/MainMenuView.swift
+++ FFEX/views/MainMenuView.swift
@@ -249,7 +249,7 @@ struct MainMenuView: View {
         let current = state.wrappedValue
         let isUnavailable = current == .unavailable || current == .checking
         let isWorking     = current == .injecting
-        let isDone        = current == .done
+
 
         Button {
             Task { await handleInject(game: game, state: state, activeSession: activeSession) }
